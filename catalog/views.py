f354fd4a1a59e653882c40b40c25008fbbfc3030
Sam Farley <sam4400@gmail.com>
2020-09-26 18:01:45 +0800

    Tutorial Part 7 Complete
    Signed-off-by: Sam Farley <sam4400@gmail.com>

--- catalog/views.py
+++ catalog/views.py
@@ -26,13 +26,18 @@ def index(request):
     # Books that contain Test
     num_books_test = Book.objects.filter(title__contains='test').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
-        'num_books_test': num_books_test
+        'num_books_test': num_books_test,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
