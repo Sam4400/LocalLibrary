5e97054778198b40b83da7cab5bbf8818e4d9662
Sam Farley <Sam4400@gmail.com>
2021-10-05 18:28:52 +0800

    2021 branch open, renew venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sam44\PycharmProjects\LocalLibrary\venv
+set VIRTUAL_ENV=C:\Users\sam44\OneDrive\Documents\GitHub\LocalLibrary\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
